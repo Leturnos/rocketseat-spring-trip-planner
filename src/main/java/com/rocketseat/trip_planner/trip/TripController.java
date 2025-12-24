6db8eb8f86185bf7d554c6241011c7fb2b09b443
Leandro da Silva Pereira <leandrodasilvapereira10@gmail.com>
2025-12-24 18:35:09 -0300

    feat: add get trip by id endpoint

--- src/main/java/com/rocketseat/trip_planner/trip/TripController.java
+++ src/main/java/com/rocketseat/trip_planner/trip/TripController.java
@@ -3,10 +3,10 @@ package com.rocketseat.trip_planner.trip;
 import com.rocketseat.trip_planner.participant.ParticipantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/trips")
@@ -27,5 +27,14 @@ public class TripController {
         this.participantService.registerParticipantsToEvent(payload.emails_to_invite(), newTrip.getId());
 
         return ResponseEntity.ok(new TripCreateResponse(newTrip.getId()));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Trip> getTripDetails(@PathVariable UUID id) {
+        // Optional, porque pode ou não existir a Trip com o id
+        Optional<Trip> trip  = this.tripRepository.findById(id);
+
+        return trip.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+
     }
 }
